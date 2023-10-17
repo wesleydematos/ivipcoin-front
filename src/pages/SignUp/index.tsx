f7a8412f5912f4f8bd95775881cbe5ec305e74db
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-17 14:35:53 -0300

    feat: created registerPage

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,107 @@
-export const SignUpPage = () => {
-    return (
-        <>SignUp</>
-    )
+import {Avatar, Button, TextField, Link, Grid, Box, Typography, Container} from "@mui/material"
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 6 }}>
+      Copyright © Gerenciador de Tarefas {new Date().getFullYear()}.
+    </Typography>
+  )
+}
+
+export const SignUpPage = () =>{
+  const handleSubmit = () => {
+    console.log("/")
+  }
+
+  return (
+    <Box
+        component="main"
+        sx={{
+            minWidth: "100vw",
+            display: "flex",
+            justifyContent: "center"
+        }}
+    >
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Registre-se
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="Nome"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Sobrenome"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Senha"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Registrar
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/" variant="body2">
+                  Já possui uma conta? Entrar!
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright/>
+      </Container>
+    </Box>
+  )
 }
