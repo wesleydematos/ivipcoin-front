21f1fccfc495cff03c0fe18ebba03bfb7c2455de
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-17 17:16:44 -0300

    fix: adjusted item name

--- src/components/AuthRoutes/index.tsx
+++ src/components/AuthRoutes/index.tsx
@@ -1,7 +1,7 @@
 import {Navigate, Outlet} from "react-router-dom"
 
 export const AuthRoutes = () => {
-  const token = localStorage.getItem("@Token-ProSupport")
+  const token = localStorage.getItem("@Token-ivipcoin")
 
   return token ? <Outlet /> : <Navigate to="/" />
 }
