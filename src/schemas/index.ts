310801e23744b5938a200475029b155dc08e0031
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-18 16:52:34 -0300

    feat: created task schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,4 +1,5 @@
 import {loginSchema} from "./loginSchema"
 import {registerSchema} from "./registerSchema"
+import {taskSchema} from "./taskSchema"
 
-export {loginSchema, registerSchema}
+export {loginSchema, registerSchema, taskSchema}
