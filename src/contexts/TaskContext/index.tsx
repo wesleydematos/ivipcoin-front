c56f44674b6002bdb4d65e490b7055f929476df2
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-10-18 09:47:12 -0300

    feat: created function to list tasks

--- src/contexts/TaskContext/index.tsx
+++ src/contexts/TaskContext/index.tsx
@@ -1,17 +1,40 @@
-import {createContext, useContext} from "react"
+import {createContext, useContext, useState} from "react"
 import {iTaskContext, iTaskContextProps} from "./interfaces"
+import { api } from "../../services/api"
+import { useUserContext } from "../UserContext"
+import {toast} from "react-toastify"
 
 const TaskContext = createContext<iTaskContext>({} as iTaskContext)
 
 export const TaskProvider = ({children}: iTaskContextProps) => {
+  const {setLoading} = useUserContext()
+  const [tasks, setTasks] = useState([])
+  
+  const getTasks = async (): Promise<void> => {
+    setLoading(true)
 
-    return (
-        <TaskContext.Provider value={{
-            
-        }}>
-            {children}
-        </TaskContext.Provider>
-    )
+    const token = localStorage.getItem("@Token-ivipcoin");
+    api.defaults.headers.common.authorization = `Bearer ${token}`
+
+    try {
+      const response = await api.get('/tasks')
+      setTasks(response.data)
+    } catch (error) {
+      toast.error("Não foi possível listar as tarefas!")
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <TaskContext.Provider value={{
+      getTasks,
+      tasks
+    }}>
+      {children}
+    </TaskContext.Provider>
+  )
 }
 
 export const useTaskContext = () => useContext(TaskContext)
--- src/contexts/TaskContext/interfaces.tsx
+++ src/contexts/TaskContext/interfaces.tsx
@@ -1,8 +1,23 @@
 import {ReactNode} from "react"
 
 export interface iTaskContext {
+  getTasks: () => Promise<void>
+  tasks: iTask[] | []
 }
 
 export interface iTaskContextProps {
-    children: ReactNode
+  children: ReactNode
+}
+
+export interface iTask {
+  id: string
+  owner: iOwner
+  description: string
+  created_at: string
+  title: string
+}
+
+interface iOwner {
+  user_id: string
+  username: string
 }
